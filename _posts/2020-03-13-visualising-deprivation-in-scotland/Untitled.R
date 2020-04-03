2708fd00962308227b2d591d302d3f3ad0c30bed
davidhen <daibhaidh.hen@gmail.com>
2020-04-03 13:01:19 +0100

    Add Covid-blog

---
+++ _posts/2020-03-13-visualising-deprivation-in-scotland/Untitled.R
@@ -0,0 +1,11 @@
+@article{noble2006measuring,
+  title={Measuring multiple deprivation at the small-area level},
+  author={Noble, Michael and Wright, Gemma and Smith, George and Dibben, Chris},
+  journal={Environment and planning A},
+  volume={38},
+  number={1},
+  pages={169--185},
+  year={2006},
+  publisher={SAGE Publications Sage UK: London, England}
+}
+
